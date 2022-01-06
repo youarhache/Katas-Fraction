747720696f2cd4df3019b7b5d23bd6734665c815
youarhache <youarhache@gmail.com>
2022-01-06 12:39:51 +0100

    can not mutate a Fraction

--- fraction.py
+++ fraction.py
@@ -1,7 +1,7 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
 class Fraction:
     numerator: int
     denominator: int
-
-    def __init__(self, numerator, denominator) -> None:
-        self.numerator = numerator
-        self.denominator = denominator
--- test_fraction.py
+++ test_fraction.py
@@ -1,3 +1,5 @@
+import pytest
+from dataclasses import FrozenInstanceError
 from fraction import Fraction
 
 
@@ -7,3 +9,10 @@ def test_can_create_fraction():
     assert isinstance(my_fraction, Fraction)
     assert my_fraction.numerator == 1
     assert my_fraction.denominator == 2
+
+
+def test_can_not_modify_fraction():
+    my_fraction = Fraction(1,2)
+
+    with pytest.raises(FrozenInstanceError) as e:
+        my_fraction.numerator = 3
